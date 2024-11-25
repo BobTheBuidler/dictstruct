b206c475e3f51603a3bf78170fd6af787faf58e7
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-11-24 21:53:27 -0400

    feat: optimize `DictStruct.__hash__`

--- dictstruct/_main.py
+++ dictstruct/_main.py
@@ -233,9 +233,7 @@ class DictStruct(Struct, dict=True):  # type: ignore [call-arg]
             raise TypeError(f"unhashable type: '{type(self).__name__}'")
         if cached_hash := self.__dict__.get("__hash__"):
             return cached_hash
-        fields = (
-            getattr(self, field_name, None) for field_name in self.__struct_fields__
-        )
+        fields = (object.__getattribute__(self, field_name) for field_name in self.__struct_fields__)
         try:
             # Skip if-checks, just try it
             try:
