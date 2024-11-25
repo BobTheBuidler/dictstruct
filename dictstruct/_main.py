3a41d876e71b0886fa3386778947cb702ce85242
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-11-24 21:54:36 -0400

    feat: optimize `DictStruct.__hash__` (#22)
    * feat: optimize `DictStruct.__hash__`
    
    * chore: `black .`
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- dictstruct/_main.py
+++ dictstruct/_main.py
@@ -234,7 +234,8 @@ class DictStruct(Struct, dict=True):  # type: ignore [call-arg]
         if cached_hash := self.__dict__.get("__hash__"):
             return cached_hash
         fields = (
-            getattr(self, field_name, None) for field_name in self.__struct_fields__
+            object.__getattribute__(self, field_name)
+            for field_name in self.__struct_fields__
         )
         try:
             # Skip if-checks, just try it
