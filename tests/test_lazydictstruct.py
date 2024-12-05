34412508af90a92dedae16d381dbf778f20dcf76
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2024-12-05 00:14:02 -0400

    fix(test): fix gh test suite (#27)

--- tests/test_lazydictstruct.py
+++ tests/test_lazydictstruct.py
@@ -1,7 +1,7 @@
 import pytest
 from msgspec import Raw, field, json
 
-from dictstruct._main import LazyDictStruct
+from dictstruct._lazy import LazyDictStruct
 
 
 class MyLazyStruct(LazyDictStruct):
