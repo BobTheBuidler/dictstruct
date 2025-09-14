9e9822c2baa69cdbb19772bb3737bef1190d077b
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2025-09-14 07:27:42 +0000

    Update _lazy.py

--- dictstruct/_lazy.py
+++ dictstruct/_lazy.py
@@ -5,7 +5,7 @@ from msgspec import UNSET
 from dictstruct._main import DictStruct
 
 
-class LazyDictStruct(DictStruct, frozen=True):  # type: ignore [call-arg]
+class LazyDictStruct(DictStruct, frozen=True):  # type: ignore [call-arg, misc]
     """
     A subclass of :class:`DictStruct` that supports Just-In-Time (JIT) decoding of field values.
 
