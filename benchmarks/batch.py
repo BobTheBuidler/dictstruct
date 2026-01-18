314e71f9647f5cc0a6ede7ca1c0bcd6ed9bfed9e
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-18 05:13:48 -0400

    Update batch.py

--- benchmarks/batch.py
+++ benchmarks/batch.py
@@ -1,6 +1,9 @@
 from typing import Any, Callable
 
+def run_50k(func: Callable[..., Any], *args: Any, **kwargs: Any) -> None:
+    for _ in range(50_000):
+        func(*args, **kwargs)
 
-def batch(iterations: int, func: Callable[..., Any], *args: Any, **kwargs: Any) -> None:
-    for _ in range(iterations):
+def run_10k(func: Callable[..., Any], *args: Any, **kwargs: Any) -> None:
+    for _ in range(10_000):
         func(*args, **kwargs)
