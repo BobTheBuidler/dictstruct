8c4fb555a8d6e17d1070e236844edeccc57ca1f7
BobTheBuidler <70677534+BobTheBuidler@users.noreply.github.com>
2026-01-18 05:17:27 -0400

    Update batch.py

--- benchmarks/batch.py
+++ benchmarks/batch.py
@@ -7,3 +7,15 @@ def run_50k(func: Callable[..., Any], *args: Any, **kwargs: Any) -> None:
 def run_10k(func: Callable[..., Any], *args: Any, **kwargs: Any) -> None:
     for _ in range(10_000):
         func(*args, **kwargs)
+
+def consume_iter(obj) -> None:
+    for _ in obj:
+        pass
+
+def consume_items(obj) -> None:
+    for _ in obj.items():
+        pass
+
+def consume_values(obj) -> None:
+    for _ in obj.values():
+        pass
